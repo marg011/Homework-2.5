39e18d587c3ab8e9cebb277d773e1695420b0a49
margo <margo171190@mail.ru>
2023-06-11 15:18:54 +0400

    Initial commit

---
+++ src/main/java/pro/sky/skyprospringexceptions/SkyproSpringExceptionsApplication.java
@@ -0,0 +1,14 @@
+package pro.sky.skyprospringexceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringExceptionsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SkyproSpringExceptionsApplication.class, args);
+	}
+
+}
